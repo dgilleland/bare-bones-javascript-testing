404045e4234ce8b350d01a9cacea9d354f9a3847
Dan Gilleland <dgilleland@nait.ca>
2024-05-13 01:59:41 -0600

    WIP #1 - Improve sample demo of loading internal scripts in HTML document

--- __tests__/samples/sample1.test.js
+++ __tests__/samples/sample1.test.js
@@ -1,29 +1,89 @@
-// @vitest-environment jsdom
-import { it, expect } from 'vitest';
+// NOTE: We are not using a global window or document in these tests
+import { describe, it, expect, beforeEach } from 'vitest';
 import { JSDOM } from 'jsdom';
 import path from 'path';
 
-it('should run scripts dangerously from HTML inside the test', () => {
+describe('HTML string with inline script within JSDOM', () => {
+    let dom = undefined;
     const html = `<body>
   <div id="content"></div>
-  <script>document.getElementById("content").append(document.createElement("hr"));</script>
+  <script>
+    const span = document.createElement("span");
+    span.innerText = "words";
+    const div = document.getElementById("content");
+    div.append(span);
+  </script>
 </body>`
-    const options = { runScripts: "dangerously" };
-    const dom = new JSDOM(html, options);
 
-    // The script will be executed and modify the DOM:
-    // console.log(dom.window.document.getElementById("content").children.length); // 1
-    const actual = dom.window.document.querySelector('hr');
-    expect(actual).toBeDefined();
-});
+    it('should have an undefined DOM to start', () => {
+        expect(dom).toBeUndefined();
+    })
 
-it('should run scripts dangerously from HTML loaded from a file', async () => {
-    const options = { runScripts: "dangerously" };
-    let dom = undefined;
-    const filePath = path.join(process.cwd(), '__tests__', 'samples', 'sample1.html');
-    await JSDOM.fromFile(filePath, options).then(result => dom = result);
-    expect(dom).toBeDefined();
-    const actual = dom.window.document.querySelector('hr');
-    expect(actual).toBeDefined();
+    describe('having script execution disabled (JSDOM default)', () => {
+        beforeEach(() => {
+            dom = new JSDOM(html);
+        });
+
+        it('should have a div without children', () => {
+            const div = dom.window.document.getElementById("content");
+            expect(div.children.length).toBe(0)
+        });
+    });
+
+    describe('running scripts dangerously', () => {
+        beforeEach(() => {
+            const options = { runScripts: "dangerously" };
+            dom = new JSDOM(html, options);
+        });
+    
+        it('should have a div with a child', () => {
+            const div = dom.window.document.getElementById("content");
+            expect(div.children.length).toBe(1)
+        });
+
+        it('should have a span with a word inside the div', () => {
+            const div = dom.window.document.getElementById("content");
+            const span = div.querySelector('span');
+            expect(span.innerText).toBe('words');
+        })
+    });
 })
 
+describe('HTML file with inline script loaded by JSDOM', async () => {
+    let dom = undefined;
+    const filePath = path.join(process.cwd(), '__tests__', 'samples', 'sample1.html');
+
+    it('should have an undefined DOM to start', () => {
+        expect(dom).toBeUndefined();
+    })
+
+    describe('having script execution disabled (JSDOM default)', async () => {
+        beforeEach(async () => {
+            await JSDOM.fromFile(filePath).then(result => dom = result);
+        });
+
+        it('should have a div without children', () => {
+            const div = dom.window.document.getElementById("content");
+            expect(div.children.length).toBe(0)
+        });
+    });
+
+    describe('running scripts dangerously', async() => {
+        beforeEach(async () => {
+            const options = { runScripts: "dangerously" };
+            await JSDOM.fromFile(filePath, options).then(result => dom = result);
+        });
+    
+        it('should have a div with a child', () => {
+            const div = dom.window.document.getElementById("content");
+            expect(div.children.length).toBe(1)
+        });
+
+        it('should have a span with more words inside the div', () => {
+            const div = dom.window.document.getElementById("content");
+            const span = div.querySelector('span');
+            expect(span.innerText).toBe('more words');
+        })
+
+    })
+});
